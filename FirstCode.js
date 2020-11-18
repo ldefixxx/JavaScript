7433e2427ea74a64953ced20cb1c784cee6b4ca1
Leon <leonmarc.schmidt1@web.de>
2020-11-18 19:10:09 +0100

    push my status

--- FirstCode.js
+++ FirstCode.js
@@ -1,1 +1,14 @@
-hier könnte Java code stehen
+let myName = "Leon";
+
+let dayOfMonth = (new Date()).getDate();
+
+let monthName = (new Date()).getMonth();
+
+let yearName = (new Date()).getFullYear();
+
+let seperator = "."
+
+
+
+console.log("Heute ist der " + dayOfMonth.toString() + seperator + monthName.toString() + seperator + yearName.toString());
+console.log(typeof seperator);
