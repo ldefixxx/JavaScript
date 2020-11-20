3c291a8df0fffd2c5cef93b001be282cc119bf9a
Leon <leonmarc.schmidt1@web.de>
2020-11-20 20:08:13 +0100

    Test

---
+++ ArrayLearning.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let student = "Max, Monika, Erika, Moritz, Anton"
+
+console.log(student.indexOf("E"))
+
+student.replace(" Monika,");
+
+console.log(student.replace(" Monika,", ""))
---
+++ Übungsaufgabe.js
@@ -0,0 +1,111 @@
+"use strict"
+// =====================================   AUFGABE   ===================================== \\
+
+// Prüfe in welchem Kurs "Paul" anwesend ist und füge seinen Freund "Luca" hinzu.
+// Entferne "Gustaf" aus seinem Kurs da dieser kurzfristig nicht mehr teilnehmen kann.
+// Stelle Sicher das "Gustaf" nicht mehr in seinem Kurs eingetrahen ist (Der Input sollte -1 sein)
+// Sortiere kurs 1 aufsteigend und kurs 2 absteigend vom Alphabet.
+// Füge "Anton" in den Kurs 1 hinzu, am Index 0, also an ersterstelle.
+
+// Lösungen findest du in Zeile 100.
+
+// =====================================   Teilnehmer   ===================================== \\
+
+let englisch1 = [
+    "Max",
+    "Raul",
+    "Paul",
+    "Gustaf"
+]
+
+let englisch1 = [
+    "Kleo",
+    "Anna",
+    "Jessica",
+    "Leon"
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variable.indexof()
+// variable.push()
+
+// variable.indexof()
+// variable.splice(INDEX, 1,)
+
+// variable.sort()
+// variable.reduce()
+
+// variable.splice(0, 0, "Anton")
